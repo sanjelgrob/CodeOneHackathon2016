509ba4748c199882911b4273850aa4f9a0784901
Gnaneshwar Rao Ayanala <gayanala@unomaha.edu>
2016-11-05 22:40:57 -0500

    Day 2 changes

---
+++ public/javascripts/shortGridData.js
@@ -0,0 +1,101 @@
+var gridData =
+[
+    {
+        "id": 0,
+        "guid": "8d1e53cb-464e-47c8-8742-068b3646b388",
+        "isActive": 'xxxxxxx5678',
+        "balance": "$1,941.51",
+        "picture": "http://placehold.it/32x32",
+        "age": 66,
+        "eyeColor": "blue",
+        "name": "Miller Gomez",
+        "gender": "male",
+        "rating": 7,
+        "quantity": 332,
+        "company": "US Bank",
+        "month": "11 February 2014",
+        "email": "millergomez@quilm.com",
+        "transport": "motorbike",
+        "preference": true,
+        "phone": "+1 (894) 565-3336",
+        "address": "916 Eldert Lane, Cawood, Tennessee, 7282",
+        "about": "Laboris tempor laborum esse culpa nostrud. Ex ad ex officia voluptate sunt duis ipsum culpa id. Mollit et labore laboris proident anim nisi do sint proident pariatur.\r\n",
+        "registered": "2014-04-18T11:45:15 -03:00",
+        "latitude": 5.80634,
+        "longitude": -133.154022,
+        "tags": [
+            "velit",
+            "Lorem",
+            "non",
+            "elit",
+            "voluptate",
+            "laborum",
+            "ipsum"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Warren Hayes"
+            },
+            {
+                "id": 1,
+                "name": "Cynthia Valdez"
+            },
+            {
+                "id": 2,
+                "name": "Tonia Downs"
+            }
+        ],
+        "greeting": "Hello, Miller Gomez! You have 6 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "id": 1,
+        "guid": "1ca20df6-6ebe-44d2-b3bd-ef6d47c56404",
+        "isActive": 'xxxxxxx6899',
+        "balance": "$2,989.69",
+        "picture": "http://placehold.it/32x32",
+        "age": 47,
+        "eyeColor": "green",
+        "name": "Guerra Cortez",
+        "gender": "male",
+        "rating": 2,
+        "quantity": 435,
+        "company": "Wells Fargo",
+        "month": "26 March 2014",
+        "email": "guerracortez@insectus.com",
+        "transport": "car",
+        "preference": false,
+        "phone": "+1 (861) 407-2735",
+        "address": "474 Preston Court, Walton, Vermont, 6140",
+        "about": "Incididunt ullamco aute laborum irure. In do dolor sit incididunt labore dolor ullamco ad ipsum Lorem sint quis. Dolore ipsum deserunt labore veniam tempor mollit enim amet in proident nisi non. Sunt in sunt nostrud qui amet veniam labore fugiat ullamco sunt. Incididunt elit ipsum laborum cupidatat labore ea veniam aliqua veniam quis minim minim esse.\r\n",
+        "registered": "2014-05-05T16:14:42 -03:00",
+        "latitude": 26.640526,
+        "longitude": -165.425396,
+        "tags": [
+            "sunt",
+            "ea",
+            "culpa",
+            "occaecat",
+            "exercitation",
+            "dolor",
+            "anim"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Jami Keller"
+            },
+            {
+                "id": 1,
+                "name": "Guthrie Hurley"
+            },
+            {
+                "id": 2,
+                "name": "Muriel Holder"
+            }
+        ],
+        "greeting": "Hello, Guerra Cortez! You have 8 unread messages.",
+        "favoriteFruit": "apple"
+    }
+]
